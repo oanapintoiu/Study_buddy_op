f0f969f15763f722480335856af4f6e8ca5faac3
Jack <jwleggett@outlook.com>
2023-06-01 12:13:30 +0100

    categories controller written

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const populateDatabase = require('./data');
-const { getAllCategories, getSubcategoriesByCategory } = require('./categoryController');
+const { getAllCategories, getSubcategoriesByCategory } = require('./contollers/categories');
 
 const app = express();
 
