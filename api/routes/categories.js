561454565ab34008bc0d5a7176d20a149c4fdc06
Jack <jwleggett@outlook.com>
2023-06-01 16:57:37 +0100

    categories route is refactored, ategroies controller has been updated with the correct path to the model

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,31 +1,9 @@
 const express = require("express");
-const Category = require("../models/category");
-const Subcategory = require("../models/subcategory");
-
 const router = express.Router();
 
-// Route for fetching all categories
-router.get("/", async (req, res) => {
-  try {
-    const categories = await Category.find();
-    res.json(categories);
-  } catch (error) {
-    console.error("Error fetching categories:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+const CategoryController = require("../controllers/categories");
 
-// Route for fetching subcategories based on category
-router.get("/:category/subcategories", async (req, res) => {
-  const { category } = req.params;
-
-  try {
-    const subcategories = await Subcategory.find({ category });
-    res.json(subcategories);
-  } catch (error) {
-    console.error("Error fetching subcategories:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+router.get("/", CategoryController.getAllCategories);
+router.get("/:category/subcategories", CategoryController.getSubcategoriesByCategory);
 
 module.exports = router;
