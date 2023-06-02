625c1fbfe0d432d548084bf4095d5fbd59f0b50b
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-06-02 19:48:00 +0100

    cookies added, user profile/user updates successfull

--- frontend/src/components/userProfile/UserProfileForm.js
+++ frontend/src/components/userProfile/UserProfileForm.js
@@ -21,7 +21,7 @@ const UserProfileForm = ({ navigate }) => {
     })
     .then((response) => {
         console.log(response.status);
-        if (response.status === 201) {
+        if (response.status === 200) {
           setSuccessMessage("Your changes have been updated successfully.");
 
           // Clear the input fields
@@ -60,17 +60,25 @@ const UserProfileForm = ({ navigate }) => {
   }
 
 
-    return (
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    navigate('/login');
+  };
+
+  return (
+    <>
       <form onSubmit={handleSubmit}>
         {successMessage && <p>{successMessage}</p>}
-          <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input placeholder="Username" id="username" type='username' value={ username } onChange={handleUsernameChange} />
-          <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-          <input placeholder="First Name" id="firstName" type='firstName' value={ firstName } onChange={handleFirstNameChange} />
-          <input placeholder="Last Name" id="lastName" type='lastName' value={ lastName } onChange={handleLastNameChange} />
+        <input placeholder="Email" id="email" type='text' value={email} onChange={handleEmailChange} />
+        <input placeholder="Username" id="username" type='username' value={username} onChange={handleUsernameChange} />
+        <input placeholder="Password" id="password" type='password' value={password} onChange={handlePasswordChange} />
+        <input placeholder="First Name" id="firstName" type='firstName' value={firstName} onChange={handleFirstNameChange} />
+        <input placeholder="Last Name" id="lastName" type='lastName' value={lastName} onChange={handleLastNameChange} />
         <input id='submit' type="submit" value="Submit" />
       </form>
-    );
-}
+      <button onClick={logout}>Logout</button>
+    </>
+  );
+};
 
 export default UserProfileForm;
