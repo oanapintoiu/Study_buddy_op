2da4bc3133bf9162d6fef378da10afb2d2592c04
Quddus Rahman <qrahman115@gmail.com>
2023-06-04 17:22:15 +0100

    added loading indicator while response from open ai api is awaited and changed button names

--- frontend/src/components/postForm/PostForm.js
+++ frontend/src/components/postForm/PostForm.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 
-const PostForm = ({ handleSubmit, handleAskAI }) => {
-    const [postText, setPostText] = useState('');
+const PostForm = ({ handleSubmit, handleAskAI, loading }) => {
+  const [postText, setPostText] = useState('');
 
-    const submitForm = (event) => {
-        event.preventDefault();
-        handleSubmit(postText);
-        setPostText('');
-    };
+  const submitForm = (event) => {
+    event.preventDefault();
+    handleSubmit(postText);
+    setPostText('');
+  };
 
-    return (
-        <form onSubmit={submitForm}>
-            <textarea
-                value={postText}
-                onChange={(e) => setPostText(e.target.value)}
-                placeholder="Ask your group or Sheldon a question!"
-            />
-            <button type="submit">Submit</button>
-            <button type="button" onClick={() => handleAskAI(postText)}>Ask AI</button>
-        </form>
-    );
+  return (
+    <form onSubmit={submitForm}>
+      <textarea
+        value={postText}
+        onChange={(e) => setPostText(e.target.value)}
+        placeholder="Ask your group or Sheldon a question!"
+      />
+      <button type="submit">Message Group</button>
+      <button type="button" onClick={() => handleAskAI(postText)}>Ask Sheldon</button>
+      {loading && <p>Sheldon is thinking...</p>}
+    </form>
+  );
 };
 
 export default PostForm;
-
