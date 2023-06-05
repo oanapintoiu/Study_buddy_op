abee19c39c8eb83da0f95fa57586f990dd6d4acd
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-06-05 15:16:04 +0100

    Tests for the side bar functionality

--- frontend/src/components/sidebar/Sidebar.cy.js
+++ frontend/src/components/sidebar/Sidebar.cy.js
@@ -1,37 +1,35 @@
 import Sidebar from "./Sidebar";
+const navigate = () => {};
 
 describe('Sidebar component', () => {
-  // beforeEach(() => {
-  //   cy.window().then((win) => {
-  //     win.localStorage.setItem('token', 'fakeToken');
-  //   });
-  //   cy.visit('/posts'); // visit the route where Sidebar component is rendered
-  // });
-
   it('should render correctly', () => {
-    cy.mount(<Sidebar />);
+    cy.mount(<Sidebar navigate={navigate} />);
     
     cy.get('.sidebar').should('be.visible');
     cy.get('.dropdown').should('be.visible');
     cy.get('.study-groups').should('be.visible');
+    cy.get('.group-title').should('be.visible');
   });
 
-  it('should go to Home page when Home button is clicked', () => {
-    window.localStorage('token', 'fakeToken')
-    cy.visit('/posts');
-    cy.get('.side-bar-item').contains('Home').click();
-    cy.url("/posts").should('include', '/posts'); 
+  it('should have a home button', () => {
+    cy.mount(<Sidebar navigate={navigate} />);
+    cy.get('.side-bar-item').contains('Home');
   });
-
-  // it('should go to Profile page when Profile button is clicked', () => {
-  //   cy.get('.side-bar-item').contains('Profile').click();
-  //   cy.url().should('include', '/profile'); // replace with the expected url when Profile is clicked
-  // });
-
-  // it('should logout when Logout button is clicked', () => {
-  //   cy.get('.side-bar-item').contains('Logout').click();
-  //   // replace with the expected behaviour when Logout is clicked, for example you may want to assert that certain elements are not visible anymore
-  // });
-
-
+  it('should have a profile button', () => {
+    cy.mount(<Sidebar navigate={navigate} />);
+    cy.get('.side-bar-item').contains('Profile');
+  });
+  it('should have a tools button dropping', () => {
+    cy.mount(<Sidebar navigate={navigate} />);
+    cy.get('.dropdown').contains('Tools');
+    
+  });
+  it('should have a study groups button', () => {
+    cy.mount(<Sidebar navigate={navigate} />);
+    cy.get('.group-title').contains('Study Groups');
+  })
+  it('should have a logout button', () => {
+    cy.mount(<Sidebar navigate={navigate} />);
+    cy.get('.side-bar-item').contains('Logout');
+  });
 });
