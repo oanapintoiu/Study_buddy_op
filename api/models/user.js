269a25c3eab65251064746e07691e683694f9eae
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-06-06 15:06:27 +0100

    added tests for user model, passing

--- api/models/user.js
+++ api/models/user.js
@@ -4,10 +4,13 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  firstName: {type: String, required: false },
-  lastName: {type: String, required: false },
+  firstName: { type: String, required: false },
+  lastName: { type: String, required: false },
   avatar: { type: String, required: false },
-  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" , default: []}],
+  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group", default: [] }],
+  category: { type: String, required: false },
+  subcategory: { type: String, required: false },
+  level: { type: String, required: false },
 });
 
 const User = mongoose.model("User", UserSchema);
