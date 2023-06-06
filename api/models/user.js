394205754d12d48eab0cc71b3dfb1ce74a6de06c
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-06-06 15:41:42 +0100

    user schema: category, subcategories and level are now arrays

--- api/models/user.js
+++ api/models/user.js
@@ -8,9 +8,9 @@ const UserSchema = new mongoose.Schema({
   lastName: { type: String, required: false },
   avatar: { type: String, required: false },
   groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group", default: [] }],
-  category: { type: String, required: false },
-  subcategory: { type: String, required: false },
-  level: { type: String, required: false },
+  category: [{ type: String, required: false }],
+  subcategory: [{ type: String, required: false }],
+  level: [{ type: String, required: false }],
 });
 
 const User = mongoose.model("User", UserSchema);
