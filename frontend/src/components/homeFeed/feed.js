a91324f5ecd6006501c26b0d1173105c397dfaae
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-06-08 23:30:15 +0100

    Commit study logo

--- frontend/src/components/homeFeed/feed.js
+++ frontend/src/components/homeFeed/feed.js
@@ -149,7 +149,10 @@ const Feed = ({ navigate }) => {
     return (
       <>
       <div className="studdybuddy">
-      <h1 className='studdybuddy-font'>Study Buddy</h1>
+        <div className='logo-container'>
+          <i className="fa-solid fa-users" style={{fontSize:'50px', color: '#5f81f2'}}></i>
+        </div>
+        <h1 className='studdybuddy-color-white studdybuddy-font'>study buddy</h1>
       </div>
       <div>
       <div>
